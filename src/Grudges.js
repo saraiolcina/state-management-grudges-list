369c7b6cae9606c3912116a4adc3e01cb95024b9
Sarai <sarai.olcina@gruposantander.com>
2022-12-01 10:23:19 +0100

    created new branch

--- src/Grudges.js
+++ src/Grudges.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Grudge from './Grudge';
 
 const Grudges = ({ grudges = [], onForgive }) => {
+  
   return (
     <section className="Grudges">
       <h2>Grudges ({grudges.length})</h2>
